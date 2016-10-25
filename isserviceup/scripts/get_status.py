739f35fa95798decd3ac380c629f295de510ab95
Marco Pazzaglia <marco@pazzaglia.me>
2016-10-25 08:59:05 +0200

    Update old script

--- isserviceup/scripts/get_status.py
+++ isserviceup/scripts/get_status.py
@@ -1,20 +1,4 @@
-from isserviceup.services.aws import AWS
-from isserviceup.services.compose import Compose
-from isserviceup.services.gcloud import GCloud
-from isserviceup.services.heroku import Heroku
-from isserviceup.services.pingdom import Pingdom
-from isserviceup.services.redislabs import RedisLabs
-from isserviceup.services.sentry import Sentry
+from isserviceup.config import config
 
-services = [
-    Heroku(),
-    GCloud(),
-    AWS(),
-    RedisLabs(),
-    Compose(),
-    Pingdom(),
-    Sentry(),
-]
-
-for service in services:
-    print('{}: status={} icon_url={}'.format(service.name, service.get_status(), service.get_icon_url()))
+for service in config.SERVICES:
+    print('{}: {}'.format(service.name, service.get_status().name))
