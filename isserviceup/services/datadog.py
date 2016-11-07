119377e08d863724a6f82096f2b7d81a118fa1a3
Ben Roeder <ben.roeder@sohonet.com>
2016-11-07 21:32:27 +0000

    fixed datadog icon jpg -> png

--- isserviceup/services/datadog.py
+++ isserviceup/services/datadog.py
@@ -4,4 +4,4 @@ from isserviceup.services.models.statuspage import StatusPagePlugin
 class DataDog(StatusPagePlugin):
     name = 'DataDog'
     status_url = 'https://status.datadoghq.com/'
-    icon_url = '/images/icons/datadog.jpg'
+    icon_url = '/images/icons/datadog.png'
