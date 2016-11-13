5d3319845d4e55066e0aed844a3ba5351c8087bc
Marco Pazzaglia <marco@pazzaglia.me>
2016-11-13 09:08:04 +0100

    Remove old scripts

--- scripts/build_frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ./frontend
-npm run build
-rm -rf ../isserviceup/static
-cp -R dist/ ../isserviceup/static/
-cd -
--- scripts/get_status.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from isserviceup.config import config
-
-for service in config.SERVICES:
-    print('{}: {}'.format(service.name, service.get_status().name))
