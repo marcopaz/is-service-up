827bdf53371e3ddd9dd0bd07971d5b0bd1561be5
Nick <nick@pleatsikas.me>
2016-12-07 01:25:09 -0800

    Removed redundant code
    Removed code that essentially checked the same thing twice.

--- isserviceup/services/vultr.py
+++ isserviceup/services/vultr.py
@@ -14,14 +14,12 @@ class Vultr(Service):
 		b = BeautifulSoup(r.content, 'html.parser')
 		div = b.find('div', {'class': 'row'})
 		status = div.findAll('i', {'class': 'zmdi'})
-		if (all(x == status[0] for x in status) 
-			and all('check-circle' in str(s) for s in status)):
+		if all('check-circle' in str(s) for s in status):
 			return Status.ok
 		elif (any('alert-circle' in str(s) for s in status) 
 			or any('close-circle' in str(s) for s in status)):
 			return Status.minor
-		elif (all(x == status[0] for x in status) 
-			and all('close-cirlce' in str(s) for s in status)):
+		elif all('close-cirlce' in str(s) for s in status):
 			return Status.critical
 		else:
 			raise Exception('unexpected status')
