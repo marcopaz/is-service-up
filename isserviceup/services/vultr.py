e61374933534143db5616e90fa5d8537edc9acc2
Nick <nick@pleatsikas.me>
2016-12-06 20:29:12 -0800

    Removed weird indentation in vultr.py

--- isserviceup/services/vultr.py
+++ isserviceup/services/vultr.py
@@ -15,13 +15,13 @@ class Vultr(Service):
 		div = b.find('div', {'class': 'row'})
 		status = div.findAll('i', {'class': 'zmdi'})
 		if (all(x == status[0] for x in status) 
-				and all('check-circle' in str(s) for s in status)):
+			and all('check-circle' in str(s) for s in status)):
 			return Status.ok
 		elif (any('alert-circle' in str(s) for s in status) 
-				or any('close-circle' in str(s) for s in status)):
+			or any('close-circle' in str(s) for s in status)):
 			return Status.minor
 		elif (all(x == status[0] for x in status) 
-				and all('close-cirlce' in str(s) for s in status)):
+			and all('close-cirlce' in str(s) for s in status)):
 			return Status.critical
 		else:
 			raise Exception('unexpected status')
